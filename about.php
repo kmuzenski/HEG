827ec04a34117aa5a88cc542890119da7aa20514
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-11-23 23:18:58 -0600

    image fix

--- about.php
+++ about.php
@@ -28,22 +28,22 @@
 
 <div class="row">
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/one.jpg">
+<img src="assets/img/one.jpg" width="200" height="200">
 </div>
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/two.jpg">
+<img src="assets/img/two.jpg" width="200" height="200">
 </div>
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/three.jpg">
+<img src="assets/img/three.jpg" width="200" height="200">
 </div>
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/four.jpg">
+<img src="assets/img/four.jpg" width="200" height="200">
 </div>
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/five.jpg">
+<img src="assets/img/five.jpg" width="200" height="200">
 </div>
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/six.jpg">
+<img src="assets/img/six.jpg" width="200" height="200">
 </div>
 
 </div>
