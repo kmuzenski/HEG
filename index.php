6dd8cd3a93c4987fdfef8851dd35a2a78f0ba61b
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-11-23 20:28:13 -0600

    index

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
 <div class="row">
 <div class="col-xs-12 col-md-12 col-lg-12">
 
-	
+<center>
 <h1> HOLLYS ENCHANTED GARDENS</h1>
 <br><br>
 <h3> extraodinary land scaping and snow removal </h3>
-
+</center>
 <br><br><br><br>
 
 
