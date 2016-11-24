f570ad0865cfc6af8dd204bce3692fe70b33e6dc
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-11-24 00:03:17 -0600

    test

--- nav.php
+++ nav.php
@@ -6,11 +6,9 @@
 		<button type="button" class="navbar-toggle btn-custom" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
          	<span class="sr-only">Toggle navigation</span>
          	<span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
         </button>
          
-         <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="50" height="50"></a>		
+         <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" id="logo"></a>		
      
      	</div>
 
