0d791385017ccaab91febc690b00652b90b8b06b
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-02-20 17:33:36 -0600

    navbar

--- header.php
+++ header.php
@@ -1,8 +1,6 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css"> 
-
 <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
 <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
