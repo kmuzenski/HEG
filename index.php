462c75b62e3abb39e5d42e4bf303a2ebf82bb71f
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-02-20 16:39:45 -0600

    adding img to index

--- index.php
+++ index.php
@@ -12,69 +12,21 @@
 
 <center>
 <h3> HOLLYS ENCHANTED GARDENS</h3>
-<h4>Exceptional Landscaping and Snow Removal Services</h4>
 </center>
+
 <br><br><br>
 </div>
 </div>
 
-<br><br><br><br>
-
-<div class="row">
-<div class="col-xs-4 col-md-4 col-lg-4">
-<h5>Gallery</h5>
-</div>
-</div>
-
-<div class="row">
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/one.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/two.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/three.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/four.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/five.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/six.jpg" width="200" height="200">
-</div>
-
-</div>
-
-<br><br><br><br>
-
-<div class="row">
-<div class="col-xs-4 col-md-4 col-lg-4">
-<h5>Services</h5>
-</div>
-</div>
-
-<div class="row">
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/1.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/2.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/3.jpg" width="200" height="200">
-</div>
-<div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/4.jpg" width="200" height="200">
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<img src="indexpot.JPG">
+		</div>
+	</div>
 </div>
 
 
-</div>
-
-
-</div>
 
 
 <br><br><br><br><br>
