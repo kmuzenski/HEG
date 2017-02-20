c803d7e0787ceb2a39058ac9f446320f33774a11
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-02-20 16:59:45 -0600

    fix

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-xs-4 col-md-6 col-lg-12">
+		<div class="col-xs-12 col-md-12 col-lg-12">
 			<img src="assets/img/indexpot.JPG" width="100%">
 		</div>
 	</div>
