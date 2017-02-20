ea1dde245d5089579ee3d846e220d6067a89cff6
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-02-20 17:28:44 -0600

    style test

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.css"> 
+
 <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
 <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
