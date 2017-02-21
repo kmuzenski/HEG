d4b05eacfb300cc4ac91b8daceee4c074313cbb4
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-02-21 09:25:56 -0600

    test

--- header.php
+++ header.php
@@ -2,7 +2,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="css/bootstrap-override.css">
-
 <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
 </head>
