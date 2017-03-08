d036d001f548681460366abc86dc168f17bfb946
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-03-07 18:44:04 -0600

    img test

--- gallery.php
+++ gallery.php
@@ -39,7 +39,7 @@
 <div class="mySlides">
   <div class="numbertext">2 / 3</div>
   <center>
-  <img src="assets/img/smpot1.JPG" style="width:50%" height="300px">
+  <img src="assets/img/smpot1.JPG" style="width:50%">
   </center>
   <div class="text">Caption Two</div>
 </div>
