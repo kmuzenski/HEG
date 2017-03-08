ac6822da624e95391c691742b7dd6082ec0fadf1
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-03-07 20:36:53 -0600

    + album page

--- albums.php
+++ albums.php
@@ -25,7 +25,7 @@
 	<div class="row">
 		<div class="col-xs-4 col-md-4 col-lg-4">
 		<center>
-			<img src="assets/img/smalbum.png" width="75%">
+			<a href="summerpots.php" title="summer pots"><img src="assets/img/smalbum.png" width="75%"></a>
 		</center>
 		</div>
 		
