5e234f7e48f5b9669c7928658b0ccee3ac8099e0
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-04-12 17:09:24 -0500

    style test

--- mynav.php
+++ mynav.php
@@ -1,16 +1,22 @@
-<div class="container">
+<div class="container" id="mynavb">
 	<div class="row">
 	
 	<div class="col-xs-12 col-md-3 col-lg-4">
-	<p id="nav"><a href="index.php" title="home">Home</a></p>
+	<center>
+	<p id="mynav"><a href="index.php" title="home">Home</a></p>
+	</center>
 	</div>
 
 	<div class="col-xs-12 col-md-4 col-lg-4">
-	<p id="nav"><a href="gallery.php" title="gallery">Gallery</a></p>
+	<center>
+	<p id="mynav"><a href="gallery.php" title="gallery">Gallery</a></p>
+	</center>
 	</div>
 
 	<div class="col-xs-12 col-md-4 col-lg-4">
-	<p id="nav"><a href="contact.php" title="contact">Contact</a></p>
+	<center>
+	<p id="mynav"><a href="contact.php" title="contact">Contact</a></p>
+	</center>
 	</div>
 
 </div> <!--row-->
