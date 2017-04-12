df44603439d9affb7dcf1bae44b43e5738905753
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-04-12 17:03:40 -0500

    testing new nav

---
+++ mynav.php
@@ -0,0 +1,17 @@
+<div class="container">
+	<div class="row">
+	
+	<div class="col-xs-12 col-md-3 col-lg-4">
+	<p id="nav"><a href="index.php" title="home">Home</a></p>
+	</div>
+
+	<div class="col-xs-12 col-md-4 col-lg-4">
+	<p id="nav"><a href="gallery.php" title="gallery">Gallery</a></p>
+	</div>
+
+	<div class="col-xs-12 col-md-4 col-lg-4">
+	<p id="nav"><a href="contact.php" title="contact">Contact</a></p>
+	</div>
+
+</div> <!--row-->
+</div> <!--container -->
