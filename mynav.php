ad9d121ef4a76dba678712ab9f2ef376a86c0346
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-04-16 18:12:05 -0500

    style

--- mynav.php
+++ mynav.php
@@ -1,19 +1,19 @@
 <div id="mynavb">
 	<div class="row">
 	
-	<div class="col-xs-12 col-md-3 col-lg-4">
+	<div class="col-xs-12 col-md-4 col-lg-4" id="navborder">
 	<center>
 	<p><a href="index.php" id="mynav" title="home">Home</a></p>
 	</center>
 	</div>
 	
-	<div class="col-xs-12 col-md-4 col-lg-4">
+	<div class="col-xs-12 col-md-4 col-lg-4" id="navborder" >
 	<center>
 	<p><a href="gallery.php" id="mynav" title="gallery">Gallery</a></p>
 	</center>
 	</div>
 
-	<div class="col-xs-12 col-md-4 col-lg-4">
+	<div class="col-xs-12 col-md-4 col-lg-4" id="navborder" >
 	<center>
 	<p><a href="contact.php" id="mynav" title="contact" id="mynav">Contact</a></p>
 	</center>
