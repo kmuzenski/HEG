6ffc27419cf17674d0777bf5b779fba02bffb2de
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-04-16 18:27:56 -0500

    style test

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-xs-12 col-md-12 col-lg-12">
-			<center>
-			<img src="assets/img/flogo.png" width="50px">
+		<div class="col-xs-4 col-md-4 col-lg-4">
+			<img src="assets/img/flogo.png" width="200px">
+		</div>
+		<div class="col-xs-8 col-md-8 col-lg-8">
 			<h4> HOLLYS <br> ENCHANTED <br> GARDENS</h4>
-			</center>
 		</div>
 	</div>
 </div>
