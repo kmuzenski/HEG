c81601c24ed7cf097bced3c95c039df278bf6d71
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-04-16 18:29:12 -0500

    style

--- index.php
+++ index.php
@@ -13,10 +13,14 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-4 col-md-4 col-lg-4">
-			<img src="assets/img/flogo.png" width="200px">
+			<center>
+			<img src="assets/img/flogo.png" width="100px">
+			</center>
 		</div>
 		<div class="col-xs-8 col-md-8 col-lg-8">
+			<center>
 			<h4> HOLLYS <br> ENCHANTED <br> GARDENS</h4>
+			</center>
 		</div>
 	</div>
 </div>
